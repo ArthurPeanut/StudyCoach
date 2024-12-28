d0a956faa69cb842d9dbf5c39e092ea00fd05a00
Haoqing Liu <u7776608@anu.edu.au>
2024-12-28 20:26:03 +1100

    Introduced CORS and context

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: process.env.CLIENT_URL,
+        origin: process.env.CLIENT_URL, // Should substitute this with frontend URL
         credentials: true,
     })
 );
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,18 +5,21 @@ import { Toaster } from 'react-hot-toast';
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+import { UserProvider } from './context/UserContext';
 
 export default function App() {
   return (
-    <ChakraProvider>
-      <BrowserRouter>
-        <Toaster position='bottom-right' />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/signin' element={<SignIn />} />
-          <Route path='/signup' element={<SignUp />} />
-        </Routes>
-      </BrowserRouter>
-    </ChakraProvider>
+    <UserProvider>
+      <ChakraProvider>
+        <BrowserRouter>
+          <Toaster position='bottom-right' />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/signin' element={<SignIn />} />
+            <Route path='/signup' element={<SignUp />} />
+          </Routes>
+        </BrowserRouter>
+      </ChakraProvider>
+    </UserProvider>
   );
 }
